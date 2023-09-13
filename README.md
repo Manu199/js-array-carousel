500170b359f41a8c5ef62468391b78e197823c29
Manu199 <manu199gh@gmail.com>
2023-09-13 15:50:17 +0200

    [add] list images

--- README.md
+++ README.md
@@ -2,7 +2,16 @@ Title
 ===
 Abstract:xxx
 ## Papar Information
-- Creare un container in cui mettere 5 immgini tramite js.
-- Togliere la classe "hide" in base all'immagine che si vuole mostrare.
-- Creare 2 "bottoni" per lo scorrimento delle immagini.
-- 
+- con un ciclo stampo tutte le immagini dentro items-wrapper.
+- prendo tutti gli elementi con la classe item e li salvo in un array.
+- al primo elemento tolgo la classe hide
+- al click di bottom 
+  - aggiungo la classe hide all'elemento corrente
+  - incremento il contatore
+  - tolgo la classe hide all'elemento corrente
+- al click di up come punto 4 ma inverso
+- nascondo di default il bottone up
+- al click di bottom appare up
+- quando sono all'ultimo elemento nascondo il bottone bottom
+- al click di up mostro bottom
+- quando sono sul primo elemento nascondo up
---
+++ js/script.js
@@ -0,0 +1,31 @@
+/*
+1- con un ciclo stampo tutte le immagini dentro items-wrapper.
+2- prendo tutti gli elementi con la classe item e li salvo in un array.
+3- al primo elemento tolgo la classe hide
+4- al click di bottom 
+  a. aggiungo la classe hide all'elemento corrente 
+  b. incremento il contatore
+  c. tolgo la classe hide all'elemento corrente
+5- al click di up come punto 4 ma inverso
+6- nascondo di default il bottone up
+7- al click di bottom appare up
+8- quando sono all'ultimo elemento nascondo il bottone bottom
+9- al click di up mostro bottom
+10- quando sono sul primo elemento nascondo up
+*/ 
+
+
+const itemsWrapper = document.querySelector('.items-wrapper');
+const btnNext = document.querySelector('.bottom');
+const btnPrev = document.querySelector('.up');
+
+
+const images = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp',
+]
+
+console.log(images)
